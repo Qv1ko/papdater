b694171d8947df47b292d15d1823930bdbf5e343
Víctor García <95133973+v1xoo@users.noreply.github.com>
2022-07-31 00:32:20 +0200

    Update papdater.sh

--- papdater.sh
+++ papdater.sh
@@ -3,7 +3,7 @@
 # Author: Víctor García (aka v1xo)
 
 function papdater(){
-	sudo apt update -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y	
+	sudo apt update -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 	sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get autoremove -y
 	sudo parrot-upgrade --fix-missing -y
 	searchsploit -u
