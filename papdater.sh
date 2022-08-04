8ed65fa8b5d26af88a05d4adebe5beea04bb95f1
Víctor García <95133973+v1xoo@users.noreply.github.com>
2022-08-04 20:55:22 +0200

    Update papdater.sh
    Added command to update nmap databases.

--- papdater.sh
+++ papdater.sh
@@ -6,6 +6,7 @@ function papdater(){
 	sudo apt update -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 	sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get autoremove -y
 	sudo parrot-upgrade --fix-missing -y
-	searchsploit -u
+	echo "\n\t[+] UPDATING THE TOOLS DATABASES [+]\n"
+	searchsploit -u; nmap --script-updatedb
 	echo "\n\n\t[+] UPDATE SUCCESSFULLY COMPLETED [+]\n"
 }
